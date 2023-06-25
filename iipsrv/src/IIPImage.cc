f4bd290e3a743f10b531cef7c4ede227c0f13663
CGDogan <126820728+CGDogan@users.noreply.github.com>
2023-06-25 19:01:05 +0100

    Revert "Revert "Revert "Revert "Ask OpenSlide about compatibility""""
    This reverts commit ea728335438949a3bbd1d4b1c83a8f6970aa56dc.

--- iipsrv/src/IIPImage.cc
+++ iipsrv/src/IIPImage.cc
@@ -40,6 +40,9 @@
 #include <ctime>
 #include <limits>
 
+extern "C" {
+#include "openslide.h"
+}
 
 using namespace std;
 
@@ -122,26 +125,9 @@ void IIPImage::testImageType() throw(file_error)
     unsigned char bbigtiff[4] = {0x49,0x49,0x2B,0x00}; // Big Endian BigTIFF
 
 
-// Compare our header sequence to our magic byte signatures
-    if (suffix=="vtif" ||
-        suffix=="svs" || 
-        suffix=="ndpi" || 
-        suffix=="mrxs" || 
-        suffix=="vms" || 
-        suffix=="scn" || 
-        suffix=="bif" || 
-        suffix=="dcm")
+    if ( openslide_detect_vendor( path.c_str() ) != NULL )
     	format = OPENSLIDE;
     // Compare our header sequence to our magic byte signatures
-    if (suffix=="vtif" ||
-        suffix=="svs" || 
-        suffix=="ndpi" || 
-        suffix=="mrxs" || 
-        suffix=="vms" || 
-        suffix=="scn" || 
-        suffix=="bif" || 
-        suffix=="dcm")
-    	format = OPENSLIDE;
     else if( memcmp( header, j2k, 10 ) == 0 ) format = JPEG2000;
     else if( memcmp( header, stdtiff, 3 ) == 0
 	     || memcmp( header, lsbtiff, 4 ) == 0 || memcmp( header, msbtiff, 4 ) == 0
