6130e8b1aad44cbf2df8e2b10e16cb409f3d7d8f
CGDogan <126820728+CGDogan@users.noreply.github.com>
2023-07-13 10:55:29 +0100

    Update BioFormatsImage.cc

--- iipsrv/src/BioFormatsImage.cc
+++ iipsrv/src/BioFormatsImage.cc
@@ -591,7 +591,7 @@ RawTilePtr BioFormatsImage::getNativeTile(const size_t tilex, const size_t tiley
     // works: "images/deleteme.tif""text123.txt"
     // doesn't work: "/images/LargeTestFile" 1G, 100m
     // 
-    const char *test = "/images/LargeTestFile100m";
+    const char *test = "/images/LargeTestFile10m";
     cerr << "but, instead, callin bfinternal_deleteme\n" << test << endl;
 
     if (bfinternal_deleteme(graal_thread, /*"/root/src/jcupitt.dcm"*/ /*"yes:/images/dcm5_dcm_conv.tif"*/ "/images/LargeTestFile") < 0)
