1589a221a5a3206e63e80e2b368f4423ce5d70a4
CGDogan <126820728+CGDogan@users.noreply.github.com>
2023-07-17 15:59:48 +0100

    Revert "Revert "use openslide""
    This reverts commit d94a3651c908f34f87ebc1844b6fd9973487c181.

--- iipsrv/src/IIPImage.cc
+++ iipsrv/src/IIPImage.cc
@@ -137,7 +137,7 @@ void IIPImage::testImageType() throw(file_error)
     if (bf_is_compatible(graal_thread_main, (char *) path.c_str())) {
       fprintf(stderr, "It is bf compatible! iipimage.cc\n");
 
-      format = BIOFORMATS;
+      format = OPENSLIDE; // TODO_DEBUG
     } else {
     fprintf(stderr, "It is not bf compatible. iipimage.cc\n");
 
