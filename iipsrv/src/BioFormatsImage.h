bae4783385e67010cce071cbd8485906223de5b7
CGDogan <126820728+CGDogan@users.noreply.github.com>
2023-07-23 11:11:33 +0100

    channels fix

--- iipsrv/src/BioFormatsImage.h
+++ iipsrv/src/BioFormatsImage.h
@@ -37,6 +37,7 @@ private:
     std::vector<int> bioformats_level_to_use, bioformats_downsample_in_level;
 
     int bytespc_internal;
+    int channels_internal;
     int pick_byte = 0; // 0 for pick first (big endian), 1 for pick last
 
     // Unimplemented methods in line with OpenslideImage.h:
