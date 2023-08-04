c4637a5bc1ca73febec7ddd11eeee8ad7dddf7f7
CGDogan <126820728+CGDogan@users.noreply.github.com>
2023-08-04 14:54:25 +0100

    Update BioFormatsInstance.cc

--- iipsrv/src/BioFormatsInstance.cc
+++ iipsrv/src/BioFormatsInstance.cc
@@ -3,5 +3,4 @@
 #include "BioFormatsThread.h"
 
 std::unique_ptr<BioFormatsThread> BioFormatsInstance::jvm =
-    //    std::make_unique<BioFormatsThread>(new BioFormatsThread());
     std::unique_ptr<BioFormatsThread>(new BioFormatsThread());
