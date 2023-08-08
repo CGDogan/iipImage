26d0443c1b269a57cfd11a3fe075b4c77d54ec5b
CGDogan <126820728+CGDogan@users.noreply.github.com>
2023-08-08 11:02:43 +0100

    Update BioFormatsInstance.h

--- iipsrv/src/BioFormatsInstance.h
+++ iipsrv/src/BioFormatsInstance.h
@@ -84,7 +84,7 @@ public:
   ~BioFormatsInstance()
   {
     char *communication_buffer =
-      bfbridge_instance_get_communication_buffer(bfinstance, NULL);
+      bfbridge_instance_get_communication_buffer(&bfinstance, NULL);
     delete[] communication_buffer;
 
     bfbridge_free_instance(&bfinstance, &thread.bflibrary);
